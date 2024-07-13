c815ee9c61c3c88df701c8aecc3d7b46869fa383
khalil mounchit <khalilmounchit@gmail.com>
2024-07-13 04:00:30 +0200

    k

--- src/apps/MyProjects/MyProjects.tsx
+++ src/apps/MyProjects/MyProjects.tsx
@@ -1,9 +1,9 @@
-// import { Animated } from "react-animated-css";
-// import { projects } from "../../data";
-// import ProjectCard from "./lib/ProjectCard";
+import { Animated } from "react-animated-css";
+import { projects } from "../../data";
+import ProjectCard from "./lib/ProjectCard";
 import { useState } from "preact/hooks";
 import ProjectInfo from "./ProjectInfo";
-// import t from "../../i18n/i18n";
+import t from "../../i18n/i18n";
 export default function MyProjects() {
     const [viewProject, setViewProject] = useState<Project|null>(null);
     if (viewProject) {
@@ -11,7 +11,7 @@ export default function MyProjects() {
     }
     return (
       <div class='h-full w-full md:rounded-b-md'>
-          {/* <div class='p-10 gap-10 text-center over text-white w-full h-full overflow-y-scroll flex flex-col pt-5 noscrollbar'>
+          <div class='p-10 gap-10 text-center over text-white w-full h-full overflow-y-scroll flex flex-col pt-5 noscrollbar'>
               <Animated animationIn="fadeInDown" animationOut="fadeOut" isVisible={true} animationInDuration={1000}>
                   <h1 class='text-4xl font-bold wavy-underline mb-3'>{t('app.myprojects.content.title')}</h1>
                   <p class='font-light text-white/70'>{t('app.myprojects.content.subtitle')}</p>
@@ -26,7 +26,7 @@ export default function MyProjects() {
                       </div>
                   })}
               </div>
-          </div> */}
+          </div>
           comming soon
       </div>
     )
